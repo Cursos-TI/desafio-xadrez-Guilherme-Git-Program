8b2b421c382367291f7d4892b2f554f4a1c318dd
Guilherme Dos Santos Maia <g.maia.estacio.directory@gmail.com>
2025-05-21 21:27:12 +0000

    conclusão desafio nivel aventureiro do desafio xadrez

--- xadrez.c
+++ xadrez.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
  
 int main() {
-    int torre = 0, bispo = 0, rainha = 0;
+    int torre = 0, bispo = 0, rainha = 0, cavalo = 0, movC = 0;
 
     printf("Movimento da Torre:\n");
     while (torre <= 4) { printf("Direita\n");
@@ -14,5 +14,11 @@ int main() {
     printf("\nMovimento da Rainha:\n");
     for (rainha = 0; rainha <= 7; rainha++) { printf("Esquerda\n"); }
     
+    printf("\nMovimento do Cavalo:\n");
+    while (cavalo < 1) { 
+    for ( movC = 0; movC < 2; movC++) { printf("Baixo\n"); }
+    printf("Esquerda\n");
+    cavalo++; }
+
     return 0;
 }
