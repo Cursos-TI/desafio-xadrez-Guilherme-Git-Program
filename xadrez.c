630e1902fcd6331403c2d40cfcc89ec0ec6978b6
Guilherme Dos Santos Maia <g.maia.estacio.directory@gmail.com>
2025-05-21 21:29:20 +0000

    conclusão do desafio nivel mestre do desafio xadrez

--- xadrez.c
+++ xadrez.c
@@ -1,24 +1,56 @@
 #include <stdio.h>
  
+void movimentotorre(int n){
+
+    if (n <= 4) { 
+        printf("Direita\n");
+        movimentotorre(n + 1);
+    }
+}
+
+void movimentobispo(int n){
+    int movB;
+    if (n <= 4) { 
+        printf("Cima\n");
+    for ( movB = 0; movB < 1; movB++) { 
+        printf("Direita\n"); }
+        movimentobispo(n + 1);
+    }
+}
+
+void movimentorainha(int n){
+
+    if (n <= 7) { 
+        printf("Esquerda\n"); 
+        movimentorainha(n + 1);
+    }
+}
+
+void movimentocavalo(int n){
+    int movC;
+
+    if (n < 1) { 
+    for ( movC = 0; movC < 2; movC++) { 
+        printf("Cima\n"); }
+        printf("Direita\n");
+        movimentocavalo(n + 1); 
+    }
+}
+
 int main() {
-    int torre = 0, bispo = 0, rainha = 0, cavalo = 0, movC = 0;
+    int torre = 0, bispo = 0, rainha = 0, cavalo = 0, movC = 0, movB = 0;
 
     printf("Movimento da Torre:\n");
-    while (torre <= 4) { printf("Direita\n");
-    torre++; }
+    movimentotorre(torre);
     
     printf("\nMovimento do Bispo:\n");
-    do { printf("Cima Direita\n");
-    bispo++; } while (bispo <= 4);
+    movimentobispo(bispo);
     
     printf("\nMovimento da Rainha:\n");
-    for (rainha = 0; rainha <= 7; rainha++) { printf("Esquerda\n"); }
+    movimentorainha(rainha);
     
     printf("\nMovimento do Cavalo:\n");
-    while (cavalo < 1) { 
-    for ( movC = 0; movC < 2; movC++) { printf("Baixo\n"); }
-    printf("Esquerda\n");
-    cavalo++; }
+    movimentocavalo(cavalo);
 
     return 0;
 }
